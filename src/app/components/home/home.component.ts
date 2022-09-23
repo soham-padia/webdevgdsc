10fa760e9487fc6464fe13cff48f8f83e7490305
Soham Padia <sohampadia10@gmail.com>
2022-09-23 22:56:03 +0530

    first commit

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,5 @@
 import {Component, Injectable, OnInit} from '@angular/core';
 import {HttpClient, HttpErrorResponse} from "@angular/common/http";
-import {lastValueFrom, map, Observable, pluck, throwError} from "rxjs";
-import {observableToBeFn} from "rxjs/internal/testing/TestScheduler";
-import {animate} from "@angular/animations";
 import {decode} from "html-entities";
 const route:string="https://opentdb.com/api.php?amount=10"
 export class Card{
@@ -20,6 +17,13 @@ export class ResponseApi{
 
   }
 }
+
+export class QandA{
+  constructor(public question:string,
+              public options:string[]) {
+  }
+}
+
 @Injectable({
   providedIn:"root"
 })
@@ -34,37 +38,66 @@ export class HomeComponent implements OnInit {
     response_code:1,
     results:[]
   };
+
+  // @ts-ignore
+  impData:QandA[]
+
   count=0
   cards:Card[]=[]
   answer: number[]=[];
   constructor(private http:HttpClient) { }
-  getAns(value:number,question:string){
-    console.log("sd",question)
-    if (value==1){
+
+  getAns(value:number,question:string) {
+    console.log("sd", question)
+    if (value == 1) {
       this.count++
     }
-    for (let i = 0; i<this.cards.length;i++){
+    /*for (let i = 0; i<this.cards.length;i++){
       if (this.cards[i].question==question){
         this.cards[i]==null
       }
-    }
+    }*/
+    let index = this.cards.findIndex(x => x.question === question)
+    this.cards[index].question=""
+    console.log("skjdasjgb",this.cards[index])
+
   }
+
   private async fetchData(){
     this.http.get<any>(route).subscribe(
       res=>{
         this.fetchedData=res
         this.fetchedData.results.map((value, index) => {
-          this.cards[index]=value
+
+          this.cards[index]=value/*
+          let question=value.question
+          let options=[value.correct_answer,...value.incorrect_answers]
+          this.impData[index]={question,options}*/
+          console.log("ok")
         })
         console.log("1222",this.cards)
+        console.log("hjsaduyj",this.impData)
       }
     );
+
   }
   ngOnInit(): void {
     this.fetchData()
+    this.getImpData()
     console.log(this.cards)
 
+
   }
+
+  async getImpData(){
+    for (let i = 0; i < this.cards.length; i++) {
+      this.impData[i].question=this.cards[i].question
+      let answers:string[]=[this.cards[i].question,...this.cards[i].incorrect_answers]
+      this.impData[i].options=answers
+    }
+    console.log("okkkkk",this.impData)
+  }
+
   decode(s: string) {
     return decode(s)
   }
